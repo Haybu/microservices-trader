8e6f86714ac1e4d6da22ef79db995b48085771a7
Haytham Mohamed <hmohamed@pivotal.io>
2018-05-15 20:41:28 -0500

    update

--- trades-web/src/main/java/io/pivotal/web/WebApplication.java
+++ trades-web/src/main/java/io/pivotal/web/WebApplication.java
@@ -1,13 +1,23 @@
 package io.pivotal.web;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -24,8 +34,28 @@ public class WebApplication {
     }
 
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	public RestTemplate restTemplate(OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
+		return new RestTemplateBuilder().interceptors(new ClientHttpRequestInterceptor() {
+			@Override public ClientHttpResponse intercept(HttpRequest request,
+					byte[] body, ClientHttpRequestExecution execution)
+					throws IOException {
+
+				OAuth2AuthenticationToken token =
+						OAuth2AuthenticationToken.class.cast(SecurityContextHolder.getContext()
+								.getAuthentication());
+
+				OAuth2AuthorizedClient client =
+						oAuth2AuthorizedClientService.loadAuthorizedClient(
+								token.getAuthorizedClientRegistrationId(),
+								token.getName()
+						);
+
+				request.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " +
+						client.getAccessToken().getTokenValue());
+
+				return execution.execute(request, body);
+			}
+		}).build();
+	}
+
 }
--- trades-web/src/main/java/io/pivotal/web/service/AccountService.java
+++ trades-web/src/main/java/io/pivotal/web/service/AccountService.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
@@ -15,12 +15,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Service
@@ -40,20 +35,22 @@ public class AccountService {
 	
 	public void createAccount(Account account) {
 		logger.debug("Saving account with userId: " + account.getUserid());
-		String status = restTemplate.postForObject("http://" + accountsService + "/account/", account, String.class);
+		String status =
+				restTemplate.postForObject("http://" + accountsService + "/account/", account, String.class);
 		logger.info("Status from registering account for "+ account.getUserid()+ " is " + status);
 	}
 	
 	public Map<String,Object> login(AuthenticationRequest request){
 		logger.debug("logging in with userId:" + request.getUsername());
-		Map<String,Object> result = (Map<String, Object>) restTemplate.postForObject("http://" + accountsService + "/login/".toString(), request, Map.class);
+		Map<String,Object> result =
+				(Map<String, Object>) restTemplate.postForObject("http://" + accountsService + "/login/".toString(), request, Map.class);
 		return result;
 	}
 	
 	//TODO: change to /account/{user}
 	public Account getAccount(OAuth2AuthenticationToken authentication) {
 		logger.debug("Looking for account with userId: " + authentication.getName());
-		Map userAttributes = this.userinfo(authentication);
+		Map userAttributes = this.userInfo(authentication);
 		return new Account(userAttributes);
 	}
 	
@@ -64,22 +61,18 @@ public class AccountService {
 	    logger.debug("Logout response: " + response.getStatusCode());
 	}
 
-	public Map userinfo(OAuth2AuthenticationToken authentication) {
+	public Map userInfo(OAuth2AuthenticationToken authentication) {
 		OAuth2AuthorizedClient authorizedClient = this.getAuthorizedClient(authentication);
-		Map userAttributes = Collections.emptyMap();
+		//Map userAttributes = Collections.emptyMap();
 		String userInfoEndpointUri = authorizedClient.getClientRegistration()
 				.getProviderDetails().getUserInfoEndpoint().getUri();
 		if (!StringUtils.isEmpty(userInfoEndpointUri)) {	// userInfoEndpointUri is optional for OIDC Clients
-			userAttributes = WebClient.builder()
-					.filter(oauth2Credentials(authorizedClient))
-					.build()
-					.get()
-					.uri(userInfoEndpointUri)
-					.retrieve()
-					.bodyToMono(Map.class)
-					.block();
+			ResponseEntity<Map> response = restTemplate
+					.exchange(userInfoEndpointUri, HttpMethod.GET, null, Map.class);
+
+			return response.getBody();
 		}
-		return userAttributes;
+		return null;
 	}
 
 	private String getOauth2UserInfoUrl(OAuth2AuthorizedClient client) {
@@ -87,20 +80,10 @@ public class AccountService {
 				.getProviderDetails().getUserInfoEndpoint().getUri();
 	}
 
-	private OAuth2AuthorizedClient getAuthorizedClient(OAuth2AuthenticationToken authentication) {
+	private OAuth2AuthorizedClient getAuthorizedClient(OAuth2AuthenticationToken token) {
 		return this.authorizedClientService.loadAuthorizedClient(
-				authentication.getAuthorizedClientRegistrationId()
-				, authentication.getName());
-	}
-
-	private ExchangeFilterFunction oauth2Credentials(OAuth2AuthorizedClient authorizedClient) {
-		return ExchangeFilterFunction.ofRequestProcessor(
-				clientRequest -> {
-					ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
-							.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
-							.build();
-					return Mono.just(authorizedRequest);
-				});
+				token.getAuthorizedClientRegistrationId()
+				, token.getName());
 	}
 	
 }
